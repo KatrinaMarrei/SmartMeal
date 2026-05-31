96550088674031803fb0348355d03b6d429773b4
ekalznamenskaya <ekalznamenskaya@edu.hse.ru>
2026-05-31 17:17:12 +0500

    Add first seed recipe images

---
+++ Migrations/20260531120948_AddFirstSeedRecipeImages.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartMeal.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFirstSeedRecipeImages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Dishes",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ImageUrl",
+                value: "/images/dishes/seeded/oatmeal-with-apple.png");
+
+            migrationBuilder.UpdateData(
+                table: "Dishes",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ImageUrl",
+                value: "/images/dishes/seeded/chicken-vegetable-soup.png");
+
+            migrationBuilder.UpdateData(
+                table: "Dishes",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ImageUrl",
+                value: "/images/dishes/seeded/baked-fish-with-vegetables.png");
+
+            migrationBuilder.UpdateData(
+                table: "Dishes",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "ImageUrl",
+                value: "/images/dishes/seeded/yogurt-with-berries.png");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Dishes",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ImageUrl",
+                value: "/images/dishes/breakfast.svg");
+
+            migrationBuilder.UpdateData(
+                table: "Dishes",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ImageUrl",
+                value: "/images/dishes/lunch.svg");
+
+            migrationBuilder.UpdateData(
+                table: "Dishes",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ImageUrl",
+                value: "/images/dishes/dinner.svg");
+
+            migrationBuilder.UpdateData(
+                table: "Dishes",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "ImageUrl",
+                value: "/images/dishes/snack.svg");
+        }
+    }
+}
